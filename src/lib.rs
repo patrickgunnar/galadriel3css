a3c04f3cb77f01e422d0a90d9cd7241f7bdf725d
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-12-20 19:13:56 -0300

    refactor: removed the unnecessary return on process_path

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,12 @@ extern crate napi_derive;
 pub mod rustal {
   pub mod blueprint;
   pub mod configatron;
+  pub mod dummy_test;
   pub mod file_reader;
   pub mod nucleus;
-  pub mod dummy_test;
 }
 
-use napi::Result;
-use rustal::configatron::{Configatron, configatron_init};
+use rustal::configatron::{configatron_init, Configatron};
 use rustal::nucleus::NUCLEUS_CONFIG;
 
 #[napi]
@@ -21,11 +20,9 @@ pub fn configatron_initializer() {
 }
 
 #[napi]
-pub fn process_path(_path: String) -> Result<()> {
+pub fn process_path(_path: String) {
   let configatron = Configatron::new();
   let data = configatron.collect_from_rust(vec!["global", "module", "ajx"]);
 
   println!("{:?}", data);
-
-  Ok(())
 }
