577e942f0da375ac2d8cce24bc70b80b60397081
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-12-20 18:46:02 -0300

    feat: added config init

--- src/lib.rs
+++ src/lib.rs
@@ -5,38 +5,27 @@ extern crate napi_derive;
 
 pub mod rustal {
   pub mod blueprint;
+  pub mod configatron;
   pub mod dummy_test;
+  pub mod file_reader;
+  pub mod nucleus;
 }
 
-use rustal::blueprint::Blueprint;
-
 use napi::Result;
+use rustal::configatron::{Configatron, configatron_init};
+use rustal::nucleus::NUCLEUS_CONFIG;
 
 #[napi]
-pub fn process_path(path: String) -> Result<()> {
-  let blueprint = Blueprint::new();
-  let msg = format!("starting to process current path: {}", blueprint.bold(path)).to_string();
+pub fn configatron_initializer() {
+  *NUCLEUS_CONFIG.lock().unwrap() = configatron_init();
+}
 
-  print!("\n");
-  blueprint.title("Galadriel3CSS Build Process".to_string());
-  blueprint.log(msg.clone());
-  blueprint.log(msg.clone());
-  blueprint.log(msg.clone());
+#[napi]
+pub fn process_path(_path: String) -> Result<()> {
+  let configatron = Configatron::new();
+  let data = configatron.collect_from_rust(vec!["global", "module", "ajx"]);
 
-  print!("\n");
-  blueprint.error(msg.clone());
-  blueprint.error(msg.clone());
-  blueprint.error(msg.clone());
-
-  print!("\n");
-  blueprint.info(msg.clone());
-  blueprint.info(msg.clone());
-  blueprint.info(msg.clone());
-
-  print!("\n");
-  blueprint.warn(msg.clone());
-  blueprint.warn(msg.clone());
-  blueprint.warn(msg.clone());
+  println!("{:?}", data);
 
   Ok(())
 }
