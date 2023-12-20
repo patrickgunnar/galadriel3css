ab7cada7c8bf9e0635aac7cc236d0bbf366fdd00
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-12-20 18:46:27 -0300

    feat: added the config initializer

--- src/cli/build/index.js
+++ src/cli/build/index.js
@@ -1,7 +1,9 @@
 const { glob } = require("glob");
-const { processPath } = require("../../../index");
+const { processPath, configatronInitializer } = require("../../../index");
 
 async function builderInit() {
+    configatronInitializer();
+    
     const files = await glob("**/*.{js,jsx,ts,tsx}", {
         cwd: process.cwd(),
         ignore: ["node_modules/**", "**/.*"],
