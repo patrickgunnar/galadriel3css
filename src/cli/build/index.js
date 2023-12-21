99afcad21bf54a674bb051f92ae2d1285e9f7cd1
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-12-21 09:45:06 -0300

    feat: added some blueprints and the configatron

--- src/cli/build/index.js
+++ src/cli/build/index.js
@@ -1,17 +1,29 @@
 const { glob } = require("glob");
-const { processPath, configatronInitializer } = require("../../../index");
+const { processPath, configatronInitializer, Blueprint, Configatron } = require("../../../index");
 
 async function builderInit() {
     configatronInitializer();
+
+    const blueprint = new Blueprint();
+    const configatron = new Configatron();
+
+    const { ignore, include } = JSON.parse(configatron.collectFromJs(["ignore", "include"]));
+    console.log("ignore: ", ignore);
+    console.log("include: ", include);
     
     const files = await glob("**/*.{js,jsx,ts,tsx}", {
         cwd: process.cwd(),
         ignore: ["node_modules/**", "**/.*"],
     });
 
+    blueprint.title("Galadriel3CSS Build Environment");
+    blueprint.info("the build environment started successfully");
+
     files.forEach((file) => {
         processPath(file);
     });
+
+    blueprint.info("the build environment just finished");
 }
 
 module.exports.builderInit = builderInit;
