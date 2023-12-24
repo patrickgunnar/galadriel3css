a31131d2bd32a57ff12afe122b41a7cdc591c32c
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-12-24 16:36:36 -0300

    feat: now the Codelyzer is collecting all objects from the createStyles' callback handler. It is collecting the objects, nested objects and targetChildren objects. It is also collecting the content of several createStyles from the same script.

--- src/rustal/codelyzer.rs
+++ src/rustal/codelyzer.rs
@@ -14,6 +14,7 @@ type ParserResult<'a, O> = IResult<&'a str, O, VerboseError<&'a str>>;
 
 pub struct Codelyzer {
   code: String,
+  pub create_styles_map: std::collections::HashMap<String, String>,
 }
 
 impl Codelyzer {
@@ -22,66 +23,16 @@ impl Codelyzer {
     let file_content = file_reader(path);
 
     match file_content {
-      Ok(content) => Codelyzer { code: content },
+      Ok(content) => Codelyzer { code: content, create_styles_map: std::collections::HashMap::new() },
       Err(_) => {
         blueprint.error("something went wrong whiling processing a file".to_string());
         blueprint.info(format!("path not processed: {}", path).to_string());
 
         Codelyzer {
-          code: "".to_string(),
+          code: "".to_string(), create_styles_map: std::collections::HashMap::new()
         }
       }
     }
-  }
-
-  fn simple_char(input: &str) -> ParserResult<&str> {
-    alt((
-      alt((tag("a"), tag("b"), tag("c"), tag("d"), tag("e"))),
-      alt((tag("f"), tag("g"), tag("h"), tag("i"), tag("j"))),
-      alt((tag("k"), tag("l"), tag("m"), tag("n"), tag("o"))),
-      alt((tag("p"), tag("q"), tag("r"), tag("s"), tag("t"))),
-      alt((tag("u"), tag("v"), tag("w"), tag("x"), tag("y"), tag("z"))),
-      alt((tag("A"), tag("B"), tag("C"), tag("D"), tag("E"))),
-      alt((tag("F"), tag("G"), tag("H"), tag("I"), tag("J"))),
-      alt((tag("K"), tag("L"), tag("M"), tag("N"), tag("O"))),
-      alt((tag("P"), tag("Q"), tag("R"), tag("S"), tag("T"))),
-      alt((tag("U"), tag("V"), tag("W"), tag("X"), tag("Y"), tag("Z"))),
-      alt((tag("0"), tag("1"), tag("2"), tag("3"), tag("4"))),
-      alt((tag("5"), tag("6"), tag("7"), tag("8"), tag("9"))),
-      alt((tag("á"), tag("à"), tag("â"), tag("ã"), tag("ä"))),
-      alt((tag("é"), tag("è"), tag("ê"), tag("ë"), tag("í"))),
-      alt((tag("ì"), tag("î"), tag("ï"), tag("ó"), tag("ò"))),
-      alt((tag("ô"), tag("õ"), tag("ö"), tag("ú"), tag("ù"))),
-      alt((tag("û"), tag("ü"), tag("ç"), tag("Á"), tag("À"))),
-      alt((tag("Â"), tag("Ã"), tag("Ä"), tag("É"), tag("È"))),
-      alt((tag("Ê"), tag("Ë"), tag("Í"), tag("Ì"), tag("Î"))),
-      alt((tag("Ï"), tag("Ó"), tag("Ò"), tag("Ô"), tag("Õ"))),
-      alt((tag("Ö"), tag("Ú"), tag("Ù"), tag("Û"), tag("Ü"), tag("Ç"))),
-    ))(input)
-  }
-
-  fn special_char(input: &str) -> ParserResult<&str> {
-    alt((
-      alt((tag("!"), tag("@"), tag("#"), tag("$"), tag("%"))),
-      alt((tag("¨"), tag("&"), tag("*"), tag("("), tag(")"))),
-      alt((tag("_"), tag("+"), tag("-"), tag("\\"), tag("/"))),
-      alt((tag("|"), tag(";"), tag(":"), tag("."), tag(">"))),
-      alt((tag("<"), tag(","), tag("~"), tag("^"), tag("?"))),
-      alt((tag("="), tag("'"), tag("\""), tag("¹"), tag("´"))),
-      alt((tag("²"), tag("³"), tag("£"), tag("¢"), tag("¬"))),
-      alt((tag("ª"), tag("º"), tag("`"), tag("["), tag("]"))),
-      alt((tag("{"), tag("}"), tag("§"), tag("©"), tag("®"))),
-      alt((tag("™"), tag("€"), tag("¥"), tag("∞"), tag("≠"))),
-    ))(input)
-  }
-
-  fn process_tokens(input: &str) -> ParserResult<&str> {
-    alt((
-      Self::clear_space,
-      Self::escape_char,
-      Self::special_char,
-      Self::simple_char,
-    ))(input)
   }
 
   fn escape_char(input: &str) -> ParserResult<&str> {
@@ -112,13 +63,21 @@ impl Codelyzer {
     alt((tag(":"),))(input)
   }
 
+  fn clear_semicolon(input: &str) -> ParserResult<&str> {
+    alt((tag(";"),))(input)
+  }
+
   fn clear_comma(input: &str) -> ParserResult<&str> {
     alt((tag(","),))(input)
   }
 
-  /*fn clear_backtick(input: &str) -> ParserResult<&str> {
+  fn clear_equals(input: &str) -> ParserResult<&str> {
+    alt((tag("="),))(input)
+  }
+
+  fn clear_backtick(input: &str) -> ParserResult<&str> {
     alt((tag("`"),))(input)
-  }*/
+  }
 
   fn identifier_double_quotes(input: &str) -> ParserResult<&str> {
     take_until("\"")(input)
@@ -147,10 +106,21 @@ impl Codelyzer {
     delimited(tag("//"), take_until("\n"), tag("\n"))(input)
   }
 
+  fn save_collected_objects(main_obj: std::collections::HashMap<&str, String>, children_obj: std::collections::HashMap<&str, String>) {
+    let mut final_map: std::collections::HashMap<&str, std::collections::HashMap<&str, String>> = std::collections::HashMap::new();
+
+    final_map.insert("main", main_obj);
+    final_map.insert("targetChildren", children_obj);
+
+    println!("{:#?}", final_map);
+  }
+
   fn process_create_styles(input: &str) -> ParserResult<&str> {
     alt((
       alt((
         tag("createStyles"),
+        tag("targetChildren"),
+        tag("function"),
         Self::clear_space,
         Self::clear_parenthesis,
         Self::clear_arrow,
@@ -159,6 +129,8 @@ impl Codelyzer {
         Self::clear_comma,
         Self::clear_curly_braces,
         Self::removes_comment,
+        Self::clear_semicolon,
+        Self::clear_equals,
       )),
       alt((Self::identifier_alpha_num, Self::identifier_string)),
     ))(input)
@@ -170,16 +142,41 @@ impl Codelyzer {
     let mut is_key_env = false;
     let mut is_nested_key_env = false;
     let mut processing_current_nested = false;
+    let mut is_target_children_env = false;
+
+    let mut parenthesis_count = 0;
+    let mut curly_braces_count = 0;
 
     let mut key = "";
     let mut nested_key = "";
 
-    let mut map = std::collections::HashMap::new();
-    let mut nested_map = std::collections::HashMap::new();
+    let mut map: std::collections::HashMap<&str, String> = std::collections::HashMap::new();
+    let mut nested_map: std::collections::HashMap<&str, String> = std::collections::HashMap::new();
+    let mut children_map: std::collections::HashMap<&str, String> = std::collections::HashMap::new();
 
     while let Ok((rest, result)) = Self::process_create_styles(code) {
-      //println!("Rest -> {:#?}", rest);
-      //println!("Result -> {:#?}", result);
+      if rest.starts_with("(") {
+        parenthesis_count += 1;
+      } else if rest.starts_with(")") {
+        parenthesis_count -= 1;
+
+        if parenthesis_count == 0 {
+          code = rest;
+          break;
+        }
+      }
+
+      if result == "targetChildren" {
+        is_target_children_env = true;
+      } else if rest.starts_with("{") && is_target_children_env {
+        curly_braces_count += 1;
+      } else if rest.starts_with("}") && is_target_children_env {
+        curly_braces_count -= 1;
+
+        if curly_braces_count == 0 {
+          is_target_children_env = false;
+        }
+      }
 
       if rest.starts_with(":") && !is_key_env {
         is_key_env = true;
@@ -187,13 +184,23 @@ impl Codelyzer {
       } else if rest.starts_with("{") && is_key_env {
         is_nested_key_env = true;
       } else if rest.starts_with("}") && is_key_env {
-        map.insert(key, format!("{:?}", nested_map).to_string());
+        if is_target_children_env {
+          children_map.insert(key, format!("{:?}", nested_map).to_string());
+        } else {
+          map.insert(key, format!("{:?}", nested_map).to_string());
+        }
+
         is_key_env = false;
         is_nested_key_env = false;
         processing_current_nested = false;
         nested_map.clear();
       } else if is_key_env && result != ":" && !result.trim().is_empty() && !is_nested_key_env {
-        map.insert(key, result.to_string());
+        if is_target_children_env {
+          children_map.insert(key, result.to_string());
+        } else {
+          map.insert(key, result.to_string());
+        }
+
         is_key_env = false;
       } else if rest.starts_with(":") && is_key_env && !processing_current_nested {
         nested_key = result;
@@ -203,19 +210,45 @@ impl Codelyzer {
         processing_current_nested = false;
       }
 
-      if rest.starts_with("targetChildren") {
-        code = "";
-      } else {
-        code = rest;
-      }
+      code = rest;
     }
 
-    println!("Properties -> \n{:#?}", map);
+    Self::save_collected_objects(map, children_map);
 
     Ok((code, ""))
   }
 
-  pub fn parser_code(&self) -> ParserResult<&str> {
+  fn clear_special(input: &str) -> ParserResult<&str> {
+    alt((
+      Self::clear_parenthesis,
+      Self::clear_curly_braces,
+      Self::clear_colon,
+      Self::clear_semicolon,
+      Self::clear_comma,
+      Self::clear_equals,
+      Self::removes_comment,
+      Self::clear_backtick,
+      alt((tag("!"), tag("@"), tag("#"), tag("$"), tag("%"))),
+      alt((tag("²"), tag("³"), tag("£"), tag("¢"), tag("¬"))),
+      alt((tag("ª"), tag("º"), tag("."), tag("["), tag("]"))),
+      alt((tag("_"), tag("+"), tag("-"), tag("\\"), tag("/"))),
+      alt((tag("™"), tag("€"), tag("¥"), tag("∞"), tag("≠"))),
+      alt((tag("<"), tag("~"), tag("^"), tag("?"), tag("®"))),
+      alt((tag("'"), tag("\""), tag("¹"), tag("´"), tag("©"))),
+      alt((tag("¨"), tag("&"), tag("*"), tag("§"), tag(">"), tag("|"))),
+    ))(input)
+  }
+
+  fn process_tokens(input: &str) -> ParserResult<&str> {
+    alt((
+      Self::clear_space,
+      Self::escape_char,
+      Self::clear_special,
+      Self::identifier_alpha_num,
+    ))(input)
+  }
+
+  pub fn parser_code(&mut self) -> ParserResult<&str> {
     let mut input = self.code.as_str();
 
     while let Ok((rest, _)) = Self::process_tokens(input) {
