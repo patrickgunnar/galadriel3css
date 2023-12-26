38e236c389cf750efe9187e774fb302423a2348f
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-12-26 18:41:20 -0300

    refactor: move it to core folder

--- src/rustal/nucleus.rs
+++
@@ -1,8 +0,0 @@
-use lazy_static::lazy_static;
-use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
-
-lazy_static! {
-  pub static ref NUCLEUS_CONFIG: Arc<Mutex<HashMap<String, serde_json::Value>>> =
-    Arc::new(Mutex::new(HashMap::new()));
-}
