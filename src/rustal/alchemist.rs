7d4328adba2b7566253df0c86b55b42cbbdcfbbc
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-12-27 15:56:17 -0300

    feat: added a handler to extract the values from the targetChildren's keys.

--- src/rustal/alchemist.rs
+++ src/rustal/alchemist.rs
@@ -2,6 +2,12 @@ use crate::core::property_core::PROPERTY_CORE;
 use crate::core::screen_core::SCREEN_CORE;
 use crate::core::selector_core::SELECTOR_CORE;
 use crate::rustal::classinator::classinator;
+use nom::{
+  branch::alt,
+  bytes::complete::{tag, take_until, take_while1},
+  sequence::delimited,
+  IResult,
+};
 use serde_json::{from_str, Value};
 use std::collections::HashMap;
 
@@ -92,174 +98,285 @@ impl Alchemist {
     }
   }
 
+  // identifies the alphanumeric sequence from the string
+  fn identifier_alpha_num(input: &str) -> IResult<&str, &str> {
+    take_while1(|c: char| c.is_alphanumeric() || c == '_')(input)
+  }
+
+  fn get_first_prop(input: &str) -> IResult<&str, &str> {
+    alt((
+      delimited(tag("("), Self::identifier_alpha_num, tag(",")),
+      delimited(tag("("), Self::identifier_alpha_num, tag(")")),
+    ))(input)
+  }
+
+  fn get_second_prop(input: &str) -> IResult<&str, &str> {
+    alt((
+      delimited(tag("(["), take_until("])"), tag("])")),
+      delimited(tag("["), take_until("]"), tag("]")),
+    ))(input)
+  }
+
+  fn get_third_prop(input: &str) -> IResult<&str, &str> {
+    alt((Self::identifier_alpha_num,))(input)
+  }
+
+  fn collects_properties(controller: String) -> Result<(String, Vec<String>, String), String> {
+    let mut input = controller.as_str();
+    let mut first_prop = "".to_string();
+    let mut second_prop = "".to_string();
+    let mut third_prop = "".to_string();
+
+    while let Ok((rest, result)) = alt((
+      Self::get_first_prop,
+      Self::get_second_prop,
+      Self::get_third_prop,
+      alt((tag("'"), tag("\""), tag(","), tag(" "), tag("("), tag(")"))),
+    ))(input)
+    {
+      if !result.trim().is_empty() {
+        if !result.contains(",")
+          && !result.contains("'")
+          && !result.contains("\"")
+          && first_prop.is_empty()
+        {
+          first_prop = result.trim().to_string();
+        } else if result.contains("'") || result.contains("\"") {
+          second_prop = result.chars().filter(|&c| c != '"' && c != '\'').collect();
+        } else if !first_prop.is_empty()
+          && !result.contains(",")
+          && !result.contains("'")
+          && !result.contains("\"")
+          && !result.contains("(")
+          && !result.contains(")")
+        {
+          third_prop = result.trim().to_string();
+        }
+      }
+
+      input = rest;
+    }
+
+    let first_prop = if first_prop == "(" {
+      "".to_string()
+    } else {
+      first_prop
+    };
+
+    let second_prop: Vec<String> = second_prop
+      .split(",")
+      .map(|s| String::from(s.trim()))
+      .collect();
+
+    Ok((first_prop, second_prop, third_prop))
+  }
+
   fn next_sibling(
     _is_modular: bool,
     _path: &str,
-    _controller: String,
+    controller: String,
     _value: &String,
   ) -> Result<(bool, String, String), String> {
+    let props = Self::collects_properties(controller);
+
     Ok((false, "".to_string(), "".to_string()))
   }
 
   fn subseq_sibling(
     _is_modular: bool,
     _path: &str,
-    _controller: String,
+    controller: String,
     _value: &String,
   ) -> Result<(bool, String, String), String> {
+    let props = Self::collects_properties(controller);
+
     Ok((false, "".to_string(), "".to_string()))
   }
 
   fn type_of(
     _is_modular: bool,
     _path: &str,
-    _controller: String,
+    controller: String,
     _value: &String,
   ) -> Result<(bool, String, String), String> {
+    let props = Self::collects_properties(controller);
+
     Ok((false, "".to_string(), "".to_string()))
   }
 
   fn direct_children(
     _is_modular: bool,
     _path: &str,
-    _controller: String,
+    controller: String,
     _value: &String,
   ) -> Result<(bool, String, String), String> {
+    let props = Self::collects_properties(controller);
+
     Ok((false, "".to_string(), "".to_string()))
   }
 
   fn attr_starts_with(
     _is_modular: bool,
     _path: &str,
-    _controller: String,
+    controller: String,
     _value: &String,
   ) -> Result<(bool, String, String), String> {
+    let props = Self::collects_properties(controller);
+
     Ok((false, "".to_string(), "".to_string()))
   }
 
   fn attr_contains(
     _is_modular: bool,
     _path: &str,
-    _controller: String,
+    controller: String,
     _value: &String,
   ) -> Result<(bool, String, String), String> {
+    let props = Self::collects_properties(controller);
+
     Ok((false, "".to_string(), "".to_string()))
   }
 
   fn attr_ends_with(
     _is_modular: bool,
     _path: &str,
-    _controller: String,
+    controller: String,
     _value: &String,
   ) -> Result<(bool, String, String), String> {
+    let props = Self::collects_properties(controller);
+
     Ok((false, "".to_string(), "".to_string()))
   }
 
   fn nth_child(
     _is_modular: bool,
     _path: &str,
-    _controller: String,
+    controller: String,
     _value: &String,
   ) -> Result<(bool, String, String), String> {
+    let props = Self::collects_properties(controller);
+
     Ok((false, "".to_string(), "".to_string()))
   }
 
   fn nth_of_type(
     _is_modular: bool,
     _path: &str,
-    _controller: String,
+    controller: String,
     _value: &String,
   ) -> Result<(bool, String, String), String> {
+    let props = Self::collects_properties(controller);
+
     Ok((false, "".to_string(), "".to_string()))
   }
 
   fn empty(
     _is_modular: bool,
     _path: &str,
-    _controller: String,
+    controller: String,
     _value: &String,
   ) -> Result<(bool, String, String), String> {
+    let props = Self::collects_properties(controller);
+
     Ok((false, "".to_string(), "".to_string()))
   }
 
   fn checked(
     _is_modular: bool,
     _path: &str,
-    _controller: String,
+    controller: String,
     _value: &String,
   ) -> Result<(bool, String, String), String> {
+    let props = Self::collects_properties(controller);
+
     Ok((false, "".to_string(), "".to_string()))
   }
 
   fn disabled(
     _is_modular: bool,
     _path: &str,
-    _controller: String,
+    controller: String,
     _value: &String,
   ) -> Result<(bool, String, String), String> {
+    let props = Self::collects_properties(controller);
+
     Ok((false, "".to_string(), "".to_string()))
   }
 
   fn focus(
     _is_modular: bool,
     _path: &str,
-    _controller: String,
+    controller: String,
     _value: &String,
   ) -> Result<(bool, String, String), String> {
+    let props = Self::collects_properties(controller);
+
     Ok((false, "".to_string(), "".to_string()))
   }
 
   fn active(
     _is_modular: bool,
     _path: &str,
-    _controller: String,
+    controller: String,
     _value: &String,
   ) -> Result<(bool, String, String), String> {
+    let props = Self::collects_properties(controller);
+
     Ok((false, "".to_string(), "".to_string()))
   }
 
   fn not(
     _is_modular: bool,
     _path: &str,
-    _controller: String,
+    controller: String,
     _value: &String,
   ) -> Result<(bool, String, String), String> {
+    let props = Self::collects_properties(controller);
+
     Ok((false, "".to_string(), "".to_string()))
   }
 
   fn visited(
     _is_modular: bool,
     _path: &str,
-    _controller: String,
+    controller: String,
     _value: &String,
   ) -> Result<(bool, String, String), String> {
+    let props = Self::collects_properties(controller);
+
     Ok((false, "".to_string(), "".to_string()))
   }
 
   fn last_child(
     _is_modular: bool,
     _path: &str,
-    _controller: String,
+    controller: String,
     _value: &String,
   ) -> Result<(bool, String, String), String> {
+    let props = Self::collects_properties(controller);
+
     Ok((false, "".to_string(), "".to_string()))
   }
 
   fn first_child(
     _is_modular: bool,
     _path: &str,
-    _controller: String,
+    controller: String,
     _value: &String,
   ) -> Result<(bool, String, String), String> {
+    let props = Self::collects_properties(controller);
+
     Ok((false, "".to_string(), "".to_string()))
   }
 
   fn descendent(
     _is_modular: bool,
     _path: &str,
-    _controller: String,
+    controller: String,
     _value: &String,
   ) -> Result<(bool, String, String), String> {
+    let props = Self::collects_properties(controller);
+
     Ok((false, "".to_string(), "".to_string()))
   }
 
