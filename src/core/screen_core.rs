76db69b7d419dbdce393f5beaa3dcf5e13d9e6ce
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-12-29 11:05:38 -0300

    refactor: improved the screen size names.

--- src/core/screen_core.rs
+++ src/core/screen_core.rs
@@ -5,56 +5,44 @@ lazy_static! {
   pub static ref SCREEN_CORE: HashMap<String, String> = {
     let mut map: HashMap<String, String> = HashMap::new();
 
-    map.insert("min4KScreen".to_string(), "#min-width: 2560px".to_string());
+    map.insert("min4K".to_string(), "#min-width: 2560px".to_string());
+    map.insert("minXLDesktop".to_string(), "#min-width: 1440px".to_string());
     map.insert(
-      "minExtraLargeDesktops".to_string(),
-      "#min-width: 1440px".to_string(),
-    );
-    map.insert(
-      "minLargeDesktops".to_string(),
+      "minLargeDesktop".to_string(),
       "#min-width: 1200px".to_string(),
     );
     map.insert(
-      "minStandardDesktops".to_string(),
+      "minStandardDesktop".to_string(),
       "#min-width: 992px".to_string(),
     );
     map.insert(
-      "minPortraitTablets".to_string(),
+      "minSmallTabletPortrait".to_string(),
       "#min-width: 768px".to_string(),
     );
     map.insert(
-      "minLargeSmartphones".to_string(),
+      "minMediumPhone".to_string(),
       "#min-width: 426px".to_string(),
     );
+    map.insert("minSmallPhone".to_string(), "#min-width: 320px".to_string());
+    map.insert("max4K".to_string(), "#max-width: 2559px".to_string());
+    map.insert("maxXLDesktop".to_string(), "#max-width: 1439px".to_string());
     map.insert(
-      "minStandardSmartphones".to_string(),
-      "#min-width: 320px".to_string(),
-    );
-    map.insert("max4KScreen".to_string(), "#max-width: 2559px".to_string());
-    map.insert(
-      "maxExtraLargeDesktops".to_string(),
-      "#max-width: 1439px".to_string(),
-    );
-    map.insert(
-      "maxLargeDesktops".to_string(),
+      "maxLargeDesktop".to_string(),
       "#max-width: 1199px".to_string(),
     );
     map.insert(
-      "maxStandardDesktops".to_string(),
+      "maxStandardDesktop".to_string(),
       "#max-width: 991px".to_string(),
     );
     map.insert(
-      "maxPortraitTablets".to_string(),
+      "maxSmallTabletPortrait".to_string(),
       "#max-width: 767px".to_string(),
     );
     map.insert(
-      "maxLargeSmartphones".to_string(),
+      "maxMediumPhone".to_string(),
       "#max-width: 480px".to_string(),
     );
-    map.insert(
-      "maxStandardSmartphones".to_string(),
-      "#max-width: 425px".to_string(),
-    );
+    map.insert("maxSmallPhone".to_string(), "#max-width: 425px".to_string());
 
     map
   };
