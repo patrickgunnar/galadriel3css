1716451605f5b3d84be33717f97c65de3941a95d
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-12-30 17:39:33 -0300

    feat: importing Gatekeeper class.

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ pub mod ast {
   pub mod stylitron;
 }
 
-use ast::stylitron::STYLITRON;
+//use ast::stylitron::STYLITRON;
 
 pub mod core {
   pub mod nucleus;
@@ -17,7 +17,7 @@ pub mod core {
 }
 
 use core::nucleus::NUCLEUS_CONFIG;
-use core::nucleus::STYLOMETRIC;
+//use core::nucleus::STYLOMETRIC;
 
 pub mod rustal {
   pub mod alchemist;
@@ -25,12 +25,16 @@ pub mod rustal {
   pub mod classinator;
   pub mod codelyzer;
   pub mod configatron;
-  pub mod file_reader;
+  pub mod gatekeeper;
+  pub mod readify;
 }
 
 use rustal::alchemist::Alchemist;
+use rustal::blueprint::Blueprint;
 use rustal::codelyzer::Codelyzer;
 use rustal::configatron::{configatron_init, Configatron};
+use rustal::readify::readify;
+use rustal::gatekeeper::Gatekeeper;
 
 use serde_json::Value;
 
@@ -41,21 +45,30 @@ pub fn configatron_initializer() {
 
 #[napi]
 pub fn process_path(path: String) {
+  let blueprint = Blueprint::new();
   let configatron = Configatron::new();
   let config = configatron.collects_from_rust(vec!["modular"]);
-  let mut codelyzer = Codelyzer::new(path.as_str());
 
-  if let Ok((_, map)) = codelyzer.parser_code() {
-    let modular = match config.get("modular") {
-      Some(Value::Bool(b)) => *b,
-      _ => false,
-    };
+  if let Ok(code) = readify(&path) {
+    let mut codelyzer = Codelyzer::new(code.clone());
 
-    let alchemist = Alchemist::new(modular);
+    if let Ok((_, map)) = codelyzer.parser_code() {
+      let modular = match config.get("modular") {
+        Some(Value::Bool(b)) => *b,
+        _ => false,
+      };
 
-    alchemist.process_objects(path.as_str(), map);
+      let alchemist = Alchemist::new(modular);
 
-    println!("{:#?}", STYLITRON.lock().unwrap());
-    println!("{:#?}", STYLOMETRIC.lock().unwrap());
+      alchemist.process_objects(path.as_str(), map);
+
+      let _gatekeeper = Gatekeeper::new(modular, path, code);
+
+      //println!("{:#?}", STYLITRON.lock().unwrap());
+      //println!("{:#?}", STYLOMETRIC.lock().unwrap());
+    }
+  } else {
+    blueprint.error("something went wrong whiling processing a file".to_string());
+    blueprint.info(format!("path not processed: {}", path).to_string());
   }
 }
