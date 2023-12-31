0d82ae5bdd72befb46bf54bde85c4464d4355808
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-12-31 12:06:32 -0300

    feat: started building the Gatekeeper's script using petgraph.

--- src/rustal/gatekeeper.rs
+++ src/rustal/gatekeeper.rs
@@ -1,13 +1,34 @@
+use petgraph::graph::{DiGraph, NodeIndex};
+
 pub struct Gatekeeper {
-    modular: bool,
-    path: String,
-    code: String,
+  graph: DiGraph<String, ()>,
 }
 
 impl Gatekeeper {
-  pub fn new(modular: bool, path: String, code: String) -> Self {
+  pub fn new() -> Self {
     Gatekeeper {
-        modular, path, code
+      graph: DiGraph::new(),
+    }
+  }
+
+  fn add_module(&mut self, module_name: &str) -> NodeIndex {
+    self.graph.add_node(module_name.to_string())
+  }
+
+  pub fn add_import(&mut self, from_module: &str, to_module: &str) {
+    let from_node = self.add_module(from_module);
+    let to_node = self.add_module(to_module);
+
+    self.graph.add_edge(from_node, to_node, ());
+  }
+
+  pub fn print_graph(&self) {
+    for node in self.graph.node_indices() {
+      println!("Module: {:?}", self.graph[node]);
+
+      for neighbor in self.graph.neighbors(node) {
+        println!("  ---> Imports: {:?}", neighbor);
+      }
     }
   }
 }
