bb17ee18ac32de528198583629440873287b662e
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-01-01 11:36:44 -0300

    refactor: added an extra map into stylometric.

--- src/core/nucleus.rs
+++ src/core/nucleus.rs
@@ -5,6 +5,6 @@ use std::sync::{Arc, Mutex};
 lazy_static! {
   pub static ref NUCLEUS_CONFIG: Arc<Mutex<HashMap<String, serde_json::Value>>> =
     Arc::new(Mutex::new(HashMap::new()));
-  pub static ref STYLOMETRIC: Arc<Mutex<HashMap<String, HashMap<String, HashMap<String, String>>>>> =
+  pub static ref STYLOMETRIC: Arc<Mutex<HashMap<String, HashMap<String, HashMap<String, HashMap<String, String>>>>>> =
     Arc::new(Mutex::new(HashMap::new()));
 }
